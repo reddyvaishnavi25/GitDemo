b8da21ebe90ab99e8cf88ed6fc1523fadc085a3e
Nikitha Reddy <nikithareddyvundyala@gmail.com>
2023-12-18 15:37:14 -0800

    commiting Bus entity

---
+++ src/Bus.java
@@ -0,0 +1,24 @@
+
+public class Bus {
+private int busId;
+private String busBrand;
+
+public Bus(int busId, String busBrand) {
+	super();
+	this.busId = busId;
+	this.busBrand = busBrand;
+}
+public int getBusId() {
+	return busId;
+}
+public void setBusId(int busId) {
+	this.busId = busId;
+}
+public String getBusBrand() {
+	return busBrand;
+}
+public void setBusBrand(String busBrand) {
+	this.busBrand = busBrand;
+}
+
+}
