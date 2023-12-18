92a9b6a4bd46697cfbdc96055a056ddcf74d26ce
Nikitha Reddy <nikithareddyvundyala@gmail.com>
2023-12-17 20:45:46 -0800

    commiting Car entity

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+
+public class Car {
+private int carId;
+private String brand;
+public Car()
+{
+	
+}
+public Car(int carId, String brand) {
+	super();
+	this.carId = carId;
+	this.brand = brand;
+}
+public int getCarId() {
+	return carId;
+}
+public void setCarId(int carId) {
+	this.carId = carId;
+}
+public String getBrand() {
+	return brand;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+
+}
