0cadd3133f0b6a3505a9fa0a797f549b37121baf
Nikitha Reddy <nikithareddyvundyala@gmail.com>
2023-12-17 19:33:23 -0800

    committing Driver file

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+public static void main(String args[])
+{
+	Car myCar =new Car();
+	myCar.setBrand("Honda");
+	myCar.setCarId(123);
+	System.out.println(myCar.getBrand() + myCar.getCarId());
+}
+}
