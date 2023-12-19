1480d80ba2a296a33fe6c4d73a5cad257e77fb39
Nikitha Reddy <nikithareddyvundyala@gmail.com>
2023-12-18 22:57:44 -0800

    commiting on to branch6

--- src/Driver.java
+++ src/Driver.java
@@ -2,9 +2,16 @@
 public class Driver {
 public static void main(String args[])
 {
+	System.out.println("Hello world");
 	Car myCar =new Car();
-	myCar.setBrand("Honda");
+	myCar.setBrand("Honda city");
 	myCar.setCarId(123);
 	System.out.println(myCar.getBrand() + myCar.getCarId());
+	Bus myBus=new Bus();
+	myBus.setBusId(456);
+	myBus.setBusBrand("Ashok");
+	System.out.println(myBus.getBusBrand() + myBus.getBusId());
+	System.out.println("Bye..!");
+	
 }
 }
